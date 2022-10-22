834de96c711912cd278ba04a3c50ef3854581df1
Jakob Unnebäck <jakob@folkdatorn.se>
2022-10-22 12:21:28 +0200

    Place mines and update numbers

--- sweeper.py
+++ sweeper.py
@@ -1,12 +1,14 @@
 import pygame
+import random
 
 pygame.init()
 font = pygame.font.Font('font.otf', 25)
 
-GRIDX = 20
-GRIDY = 20
+GRIDX = 15
+GRIDY = 15
 TILESIZE = 30
 SPEED = 60
+NUMBEROFMINES = 40
 
 class SweeperGame:
     def __init__(self):
@@ -32,28 +34,50 @@ class SweeperGame:
 
     def draw(self):
         self.screen.fill((0, 0, 0))
-
-        self._drawTile(0, 0)
-
         # draw tiles from map
         for x in range(GRIDX):
             for y in range(GRIDY):
-                self._drawTile(x, y, "0")
+                self._drawTile(x, y, self.map[y][x])
 
 
         pygame.display.flip()
           
-    def _drawTile(self, x, y, text="0"):
+    def _drawTile(self, x, y, text=0):
         # draw tile at x, y with size TILESIZE
+        if text == 9:
+            text = 'B'
+
         pygame.draw.rect(self.screen, (255, 255, 255), (x * TILESIZE, y * TILESIZE, TILESIZE, TILESIZE), 1)
-        # draw a number in the middle of the tile
-        text = font.render(text, True, (255, 255, 255))
-        # reduce font size
-        self.screen.blit(text, (x * TILESIZE + TILESIZE / 2 - text.get_width() // 2, y * TILESIZE + TILESIZE / 2 - text.get_height() // 2))
+        textElement = font.render(str(text), True, (255, 255, 255))
+        self.screen.blit(textElement, (x * TILESIZE + TILESIZE / 2 - textElement.get_width() // 2, y * TILESIZE + TILESIZE / 2 - textElement.get_height() // 2))
+
+    def _calculateTileNumber(self, x, y, map):
+        number = 0
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if x + i < 0 or x + i >= GRIDX or y + j < 0 or y + j >= GRIDY:
+                    continue
+                if map[y + j][x + i] == 9:
+                    number += 1
+        return number
 
     def _generateMap(self):
         # create array with size GRIDX * GRIDY
         map = [[0 for x in range(GRIDX)] for y in range(GRIDY)]
+
+        # place mines
+        for i in range(NUMBEROFMINES):
+            x = random.randint(0, GRIDX - 1)
+            y = random.randint(0, GRIDY - 1)
+            map[y][x] = 9
+
+        # calculate numbers
+        for x in range(GRIDX):
+            for y in range(GRIDY):
+                if map[y][x] == 9:
+                    continue
+                map[y][x] = self._calculateTileNumber(x, y, map)
+
         print(map)
         return map
 
